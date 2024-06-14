d8b94ffa591251ea69c9329e52151c5b432ba669
devvk <info@sekretov.net>
2024-06-14 16:55:59 +0200

    chess LogicTest

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -13,25 +13,22 @@ public class LogicTest {
 
     @Test
     public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            throws ImpossibleMoveException {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
-            logic.move(Cell.C1, Cell.H6);
-        });
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class,
+                () -> logic.move(Cell.C1, Cell.H6));
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
     @Test
-    public void whenMoveThenImpossibleMoveException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveThenImpossibleMoveException() throws ImpossibleMoveException {
         Logic logic = new Logic();
         Cell startPosition = Cell.C1;
         Figure bishopBlack = new BishopBlack(startPosition);
         logic.add(bishopBlack);
         Cell newPosition = Cell.H5;
-        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
-            logic.move(startPosition, newPosition);
-        });
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class,
+                () -> logic.move(startPosition, newPosition));
         assertThat(exception.getMessage()).isEqualTo(
                 String.format("Could not move by diagonal from %s to %s", startPosition, newPosition
                 )
@@ -39,8 +36,7 @@ public class LogicTest {
     }
 
     @Test
-    public void whenMoveThenOccupiedCellException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveThenOccupiedCellException() throws ImpossibleMoveException {
         Logic logic = new Logic();
         Cell startPosition = Cell.C1;
         Figure bishopBlack = new BishopBlack(startPosition);
@@ -48,8 +44,6 @@ public class LogicTest {
         Cell newPosition = Cell.E3;
         Figure queenBlack = new QueenBlack(newPosition);
         logic.add(queenBlack);
-        assertThrows(OccupiedCellException.class, () -> {
-            logic.move(startPosition, newPosition);
-        });
+        assertThrows(OccupiedCellException.class, () -> logic.move(startPosition, newPosition));
     }
 }
