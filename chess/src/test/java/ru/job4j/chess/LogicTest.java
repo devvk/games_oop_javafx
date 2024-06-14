31b08cb3070d739e045c99e83bbc99b2ffca5465
devvk <info@sekretov.net>
2024-06-14 16:52:58 +0200

    chess LogicTest

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,14 +1,14 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.QueenBlack;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -19,5 +19,37 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenImpossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        Cell startPosition = Cell.C1;
+        Figure bishopBlack = new BishopBlack(startPosition);
+        logic.add(bishopBlack);
+        Cell newPosition = Cell.H5;
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(startPosition, newPosition);
+        });
+        assertThat(exception.getMessage()).isEqualTo(
+                String.format("Could not move by diagonal from %s to %s", startPosition, newPosition
+                )
+        );
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        Cell startPosition = Cell.C1;
+        Figure bishopBlack = new BishopBlack(startPosition);
+        logic.add(bishopBlack);
+        Cell newPosition = Cell.E3;
+        Figure queenBlack = new QueenBlack(newPosition);
+        logic.add(queenBlack);
+        assertThrows(OccupiedCellException.class, () -> {
+            logic.move(startPosition, newPosition);
+        });
     }
 }
